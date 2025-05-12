8f9ec07e6633a9b315aa9dcb16275b1659dd8a44
Sourav Maharana <saurav.maharana07@gmail.com>
2025-05-12 10:50:06 +0530

    Added numbering convention

---
+++ AwesomeProject01/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  extends: '@react-native',
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};
